a9394d11792e73b9a2cd48928f0e68da22533d74
Johnson <shunmian@gmail.com>
2019-12-01 13:00:56 +0800

    spacemacs set helm-locate-fuzzy-match

--- Mackup/.spacemacs.d/init.el
+++ Mackup/.spacemacs.d/init.el
@@ -314,6 +314,7 @@ This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
   (popwin-mode 1)
+  (setq helm-locate-fuzzy-match t)
   )
 
 ;; Do not write anything past this comment. This is where Emacs will

5e95f754988bd15102a4de5a303f259a369d2cbd
Johnson <shunmian@gmail.com>
2019-12-03 10:15:27 +0800

    add shell

--- Mackup/.doom.d/config.el
+++ Mackup/.doom.d/config.el
@@ -3,3 +3,7 @@
 ;; Place your private configuration here
 
 (fset 'battery-update #'ignore)
+(setq explicit-shell-file-name "/bin/zsh")
+(map! (:leader
+        (:desc "open shell" :g "oe" #'ansi-term)) ;; 这个快捷键是SPC o e
+)

e3a7d8059c8e02b188e6c7025a3a12a1185d5b93
Johnson <shunmian@gmail.com>
2019-12-03 10:18:00 +0800

    doompaces open zsh automatically by SPC o e

--- Mackup/.doom.d/config.el
+++ Mackup/.doom.d/config.el
@@ -4,6 +4,13 @@
 
 (fset 'battery-update #'ignore)
 (setq explicit-shell-file-name "/bin/zsh")
+(defun my/term ()
+  "My personal term command."
+  (interactive)
+  (set-buffer (make-term "terminal" explicit-shell-file-name))
+  (term-mode)
+  (term-char-mode)
+  (switch-to-buffer "*terminal*"))
 (map! (:leader
-        (:desc "open shell" :g "oe" #'ansi-term)) ;; 这个快捷键是SPC o e
+        (:desc "open shell" :g "oe" #'my/term)) ;; 这个快捷键是SPC o e
 )

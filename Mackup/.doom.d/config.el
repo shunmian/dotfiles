a93436eab6f2eb4d148f51ff8e9034fe28ee99ae
Johnson <shunmian@gmail.com>
2019-12-03 22:12:31 +0800

    doomemacs add winum

--- Mackup/.doom.d/config.el
+++ Mackup/.doom.d/config.el
@@ -38,3 +38,20 @@
 (setq display-line-numbers-type 'relative)
 (setq doom-theme 'doom-peacock)
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
+
+(winum-mode)
+
+(map! (:leader
+        (:desc "open shell" :g "0" #'treemacs-select-window))
+)
+(map! :leader
+  "1" 'winum-select-window-1
+  "2" 'winum-select-window-2
+  "3" 'winum-select-window-3
+  "4" 'winum-select-window-4
+  "5" 'winum-select-window-5
+  "6" 'winum-select-window-6
+  "7" 'winum-select-window-7
+  "8" 'winum-select-window-8
+  "9" 'winum-select-window-9
+  )

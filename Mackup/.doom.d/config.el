2c29ecce9ee60e165c0614fbcd1e6b2f34976c6f
Johnson <shunmian@gmail.com>
2019-12-03 12:04:58 +0800

    doomemacs install agenda pacakge

--- Mackup/.doom.d/config.el
+++ Mackup/.doom.d/config.el
@@ -7,10 +7,31 @@
 (defun my/term ()
   "My personal term command."
   (interactive)
-  (set-buffer (make-term "terminal" explicit-shell-file-name))
+  (set-buffer (make-term "terminal" explicit-shell-file-name projectile-project-root))
   (term-mode)
   (term-char-mode)
   (switch-to-buffer "*terminal*"))
 (map! (:leader
         (:desc "open shell" :g "oe" #'my/term)) ;; 这个快捷键是SPC o e
 )
+
+(def-package! org-super-agenda
+  :after org-agenda
+  :init
+  (setq org-super-agenda-groups '((:name "Today"
+                                         :time-grid t
+                                         :scheduled today)
+                                  (:name "Due today"
+                                         :deadline today)
+                                  (:name "Important"
+                                         :priority "A")
+                                  (:name "Overdue"
+                                         :deadline past)
+                                  (:name "Due soon"
+                                         :deadline future)
+                                  (:name "Big Outcomes"
+                                         :tag "bo")
+                                  ))
+  :config
+  (org-super-agenda-mode)
+  )

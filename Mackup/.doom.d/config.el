f62cdab718fe839b9d4ba551f91d4f02d2c79036
Johnson <shunmian@gmail.com>
2019-12-04 00:25:13 +0800

    doomemacs update comment for dir tree focus

--- Mackup/.doom.d/config.el
+++ Mackup/.doom.d/config.el
@@ -42,7 +42,7 @@
 (winum-mode)
 
 (map! (:leader
-        (:desc "select dir-tree window" :g "0" #'treemacs-select-window))
+        (:desc "Select dir-tree window" :g "0" #'treemacs-select-window))
 )
 (map! :leader
   "1" 'winum-select-window-1
